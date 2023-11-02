dbb4d9deabf09622d3397ee6ff0e793ef47e129e
dsa04156 <dsa04156@naver.com>
2023-11-02 19:00:30 +0900

    feat : mysql 연결

---
+++ be/blockfileextension/src/main/java/com/flow/blockfileextension/BlockFileExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.flow.blockfileextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication()
+public class BlockFileExtensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockFileExtensionApplication.class, args);
+	}
+}

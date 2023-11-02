9b1cc34cc57e5d97e12daa4783bdf7f66028d65f
dsa04156 <dsa04156@naver.com>
2023-11-02 23:13:30 +0900

    feat : dto, entity 생성

---
+++ be/blockfileextension/src/main/java/com/flow/blockfileextension/domain/entity/ExtentionNameEntity.java
@@ -0,0 +1,10 @@
+package com.flow.blockfileextension.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ExtentionNameEntity {
+    private String extentionName;
+}
